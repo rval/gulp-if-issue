23ed38006a5256b97cfa4e4255d90b2536f5e626
René Valois <rene.valois@gmail.com>
2015-11-17 13:16:41 -0800

    Simplified repro steps.

--- index.js
+++ index.js
@@ -3,16 +3,15 @@
  * Reproduction steps:
  *
  *  1) Run “gulp watch” from the command line.
- *  2) Make changes to this file. Notice the watch callback executes as expected.
- *  3) Now add a syntax error to index.js.
- *  4) Gulp Plumber logs the error to the console.
- *  5) Fix the syntax error in index.js.
- *  6) Notice that the gulp-watch callback executes, but index.js is no longer transformed by Babel.
- *  7) Remove gulp-if from gulpfile.js and retry all of the above steps.
- *  8) Notice that Babel continues to build files after correcting the syntax error.
+ *  2) Make changes to index.js. Notice the watch callback executes as expected.
+ *  3) Add a syntax error to index.js. Babel produces an error, which is logged by Gulp Plumber.
+ *  4) Fix the syntax error in index.js and save the file.
+ *  5) Notice the watch callback continues to execute, but index.js is no longer transformed by Babel.
+ *
+ * If gulp-if is removed from the stream, Babel continues to build files after syntax errors. 
  */
 
-console.log('this is index.js'); 
+console.log('this is index.js');
 
 // Uncomment for a syntax error:
-// ^
+//^
